885743a677c67183f17c7b270c46a74bc7305043
guiml <guilherme@gmail.com>
2025-11-21 17:04:56 -0500

    api bogus

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def main():
 
     summary_template = """ 
     Given the information {information} about the airline, I want you to create:
-    1. A short summary
-    2. Two interesting facts about the airline
+    1. A short summary.
+    2. Two interesting facts about the airline.
     """
 
     summary_prompt_template = PromptTemplate(
