65ce29a53f93cde9cf2c5ecffb7de7e620e06ff6
guiml <guilherme@gmail.com>
2025-11-21 16:14:12 -0500

    second setup

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+import os
+
+load_dotenv()
+
+
+
 def main():
     print("Hello from langchain-course!")
+    print(os.environ.get("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
